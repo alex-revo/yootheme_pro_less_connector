25472902448c25c3f9a9c774a7651a1510b81cdd
alex-revo <alex-revo@webhleb.ru>
2021-11-20 03:47:58 +0300

    update

--- bootstrap.php
+++ bootstrap.php
@@ -20,17 +20,19 @@ if (is_dir($folder)) {
 }
 
 //echo $components;
-
+$json = '{"theme":{"styles":{"components":{"custom":"' . JPATH_SITE . '/templates/yootheme_child/less/custom.less","revo":"' . JPATH_SITE . '/templates/yootheme_child/less/revo.less"}}}}';
+//echo json_decode($json, true);
 if ($prefix && is_dir($folder)) {
-    return [
-        'theme' => [
-            'styles' => [
-                'components' => [
-                    'my-component' => (JPATH_SITE . '/templates/yootheme_' . $prefix . '/less/custom.less'),
-                ],
-            ],
-        ],
-    ];
+    // return [
+    //     'theme' => [
+    //         'styles' => [
+    //             'components' => [
+    //                 'my-component' => (JPATH_SITE . '/templates/yootheme_' . $prefix . '/less/custom.less'),
+    //             ],
+    //         ],
+    //     ],
+    // ];
+    return json_decode($json, true);
 } else {
     return [];
 }
